2b51563220cd5a441937936a2d1be9bee74fd2d4
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-11-09 09:18:40 -0600

    made image section responsive

--- src/Components/UI Elements/BgImage.jsx
+++ src/Components/UI Elements/BgImage.jsx
@@ -3,7 +3,9 @@ export default function BgImage({ imgPath }){
 
     return(
         <section className="img-section">
+           
             <img src={imgPath} alt="guitar background"></img>
+            
         </section>
     )
 }
