30697b882d0eb7d74193a8201d915819844b22ba
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-11-10 17:18:32 -0600

    added gradient capability to bio component

--- src/Components/HomeBio.jsx
+++ src/Components/HomeBio.jsx
@@ -2,7 +2,7 @@ export default function HomeBio({ imgPath, bgColor, text, title }) {
 
 
     return (
-        <section className="home-bio" style={{ backgroundColor: bgColor }}>
+        <section className="home-bio" style={{ background: bgColor }}>
             <div className="text-paragraph">
                 <h2>{title}</h2>
                 <p>{text}</p>
